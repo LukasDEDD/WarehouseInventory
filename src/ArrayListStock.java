0222e1ec11959f67b7eabafa1d0f09fe24c303fd
LukasDD82 <lukas.simek.de@gmail.com>
2025-06-18 00:35:37 +0200

    Array list. Fertig

--- src/ArrayListStock.java
+++ src/ArrayListStock.java
@@ -3,5 +3,62 @@ import java.util.List;
 
 public class ArrayListStock {
 
-    List<ArrayListStock> productNames  = new ArrayList<>();
+    private String nameLaptop;
+    private String nameTelefon;
+    private String nameMonitor;
+    private String nameKlavesnice;
+    private String nameMys;
+    private String Sluchatka;
+
+    public String getSluchatka() {
+        return Sluchatka;
+    }
+
+    public ArrayListStock(String sluchatka) {
+        Sluchatka = sluchatka;
+    }
+
+    public String getNameLaptop() {
+        return nameLaptop;
+    }
+
+    public String getNameTelefon() {
+        return nameTelefon;
+    }
+
+    public String getNameMonitor() {
+        return nameMonitor;
+    }
+
+    public String getNameKlavesnice() {
+        return nameKlavesnice;
+    }
+
+    public String getNameMys() {
+        return nameMys;
+    }
+
+    @Override
+    public String toString() {
+        return "ArrayListStock{" +
+                "nameLaptop='" + nameLaptop + '\'' +
+                ", nameTelefon='" + nameTelefon + '\'' +
+                ", nameMonitor='" + nameMonitor + '\'' +
+                ", nameKlavesnice='" + nameKlavesnice + '\'' +
+                ", nameMys='" + nameMys + '\'' +
+                ", Sluchatka='" + Sluchatka + '\'' +
+                '}';
+    }
+
+    public ArrayListStock(String nameLaptop, String nameTelefon, String nameMonitor, String nameKlavesnice, String nameMys) {
+        this.nameLaptop = nameLaptop;
+        this.nameTelefon = nameTelefon;
+        this.nameMonitor = nameMonitor;
+        this.nameKlavesnice = nameKlavesnice;
+        this.nameMys = nameMys;
+
+
+
+
+    }
 }
