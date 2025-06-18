dd0929213ab8eea1cf4d3618f611f378d16d3655
LukasDD82 <lukas.simek.de@gmail.com>
2025-06-18 17:42:09 +0200

    Final

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayField s = new ArrayField();
+
 
         ArrayListStock productNames = new ArrayListStock("laptop", "telefon", "monitor", "klavesnice", "mys");
         ArrayListStock item = new ArrayListStock("sluchatka");
